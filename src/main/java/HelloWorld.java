1e34f7c9512ed41d1ffbd962203a4b79f138736a
root <root@localhost.localdomain>
2017-08-18 23:32:22 +0800

    add hello

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+package main.java.com.lcb.mvnbook;  
+  
+public class HelloWorld {  
+    public String sayHello(){  
+        return "Hello";  
+    }     
+    public static void main(String[] args) {  
+        System.out.println(new HelloWorld().sayHello());  
+    }  
+}  
