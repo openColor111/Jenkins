34fe4ce937a341f6dea2fad048a848dfa38965f1
root <root@localhost.localdomain>
2017-08-19 00:29:44 +0800

    add daima

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld {
     }     
     public static void main(String[] args) {  
         System.out.println(new HelloWorld().sayHello());  
-	System.out.println("kaka");
+	System.out.println("kakapp");
     }  
 }  
